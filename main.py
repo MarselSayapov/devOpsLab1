f47de2af4d1b33c5979fa8b239af3a9adb82b5a0
Sayapov Marsel <122860952+MarselSayapov@users.noreply.github.com>
2024-10-19 21:20:57 +0500

    added guess number game

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-﻿print("Hello, World!")
+﻿def guess_number():
+    print("Добро пожаловать в игру 'Угадай число'!")
+    secret_number = 42  # Установим число для угадывания
+    attempts = 0
+
+    while True:
+        try:
+            guess = int(input("Угадайте число от 1 до 100: "))
+            attempts += 1
+            if guess < secret_number:
+                print("Загаданное число больше.")
+            elif guess > secret_number:
+                print("Загаданное число меньше.")
+            else:
+                print(f"Поздравляю! Вы угадали число за {attempts} попыток.")
+                break
+        except ValueError:
+            print("Пожалуйста, введите число.")
+
+
+guess_number()
